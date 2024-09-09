7b72dde1f79b3ec48a2549a71781e4a0fc4a4ff3
vijayvenkatj <143727980+vijayvenkatj@users.noreply.github.com>
2024-09-09 20:56:41 +0530

    Update route.ts

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function POST(request: NextRequest){
+export function PUT(request: NextRequest){
     const res = NextResponse.redirect(new URL('/login',request.url))
     res.cookies.set('token', '', { maxAge: 0, path: '/' });
     return res
